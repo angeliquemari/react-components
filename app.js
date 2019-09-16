b0264f9b6cb9f9a8d4413ca7d4f2eb79ce4e638a
Angelique Nehmzow <angelique@nehmzow.com>
2019-09-16 12:59:40 -0700

    add groceryList component and dynamic item component

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// TODO
+var GroceryListItem = (props) => {
+    return <li>{props.groceryItem}</li>;
+}
+
+var GroceryList = () => {
+    const groceryItems = ['Cucumbers', 'Kale'];
+    return (
+      <ul>
+        {groceryItems.map((groceryItem) => <GroceryListItem key={groceryItem} groceryItem={groceryItem} />)}
+      </ul>
+    );
+}
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
